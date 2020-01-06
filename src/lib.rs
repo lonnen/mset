763ec2d1ae36d19dc5b7f6b674ced05bc61a9dce
lonnen <chris.lonnen@gmail.com>
2020-01-06 01:18:33 -0800

    let's commence

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,43 @@
+use std::collections::HashMap;
+use std::hash::Hash;
 
+#[derive(Clone, Default, Eq)]
+pub struct MultiSet<K>
+where
+    K: Eq + Hash,
+{
+    elem_counts: HashMap<K, usize>,
+    size: usize,
+}
+
+impl<K> MultiSet<K>
+where
+    K: Eq + Hash,
+{
+    pub fn new() -> Self {
+        MultiSet {
+            elem_counts: HashMap::new(),
+            size: 0,
+        }
+    }
+}
+
+impl<K> PartialEq for MultiSet<K>
+where
+    K: Eq + Hash,
+{
+    fn eq(&self, other: &MultiSet<K>) -> bool {
+        // TODO: Fix this
+        return true
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn new() {
+        let mset: MultiSet<i32> = MultiSet::new();
     }
 }
