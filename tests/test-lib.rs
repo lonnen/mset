8281e7f7f22677799e969055593f032d4cebb9ac
lonnen <chris.lonnen@gmail.com>
2020-11-12 17:21:17 +0000

    fixup the retain tests

--- tests/test-lib.rs
+++ tests/test-lib.rs
@@ -409,10 +409,11 @@ fn test_retain() {
     assert_eq!(mset.get(&1), Some(&2usize));
     assert_eq!(mset.get(&2), Some(&2usize));
 
+    let mut mset: MultiSet<_> = [1, 2, 3, 4, 5, 4, 3, 2, 1].iter().cloned().collect();
     mset.retain(|&_k, v| {
         v == 1
     });
 
     assert_eq!(mset.elements().len(), 1);
-    assert_eq!(mset.get(&2), Some(&1usize));
+    assert_eq!(mset.get(&5), Some(&1usize));
 }
